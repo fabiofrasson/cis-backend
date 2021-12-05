9dbdc1b987e0c14883b869fad34da73a663015bb
Fabio Frasson <fabio.frass@gmail.com>
2021-12-05 15:25:27 -0300

    Specialty creation

--- src/main/java/com/cis/repository/PatientRepository.java
+++ src/main/java/com/cis/repository/PatientRepository.java
@@ -1,10 +1,8 @@
 package com.cis.repository;
 
 import com.cis.model.Patient;
-import com.cis.model.dto.PatientReturnDTO;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
 import java.util.UUID;
 
 public interface PatientRepository extends JpaRepository<Patient, UUID> {
