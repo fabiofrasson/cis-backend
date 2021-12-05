25b60578a1ae7dbaa3e4ac93b485a866151c7417
Fabio Frasson <fabio.frass@gmail.com>
2021-12-05 09:04:21 -0300

    AddressService tests

--- src/test/java/com/cis/util/AddressCreator.java
+++ src/test/java/com/cis/util/AddressCreator.java
@@ -15,22 +15,22 @@ public class AddressCreator {
     switch (option) {
       case 1:
         Address address = CepService.convertCepToAddress("85857600");
-        address.setId(UUID.randomUUID());
+        address.setId(UUID.fromString("9171004e-fb07-4cb1-bbbd-6010418481d5"));
         return address;
 
       case 2:
         Address address1 = CepService.convertCepToAddress("85851-010");
-        address1.setId(UUID.randomUUID());
+        address1.setId(UUID.fromString("9171004e-fb07-4cb1-bbbd-6010418481d6"));
         return address1;
 
       case 3:
         Address address2 = CepService.convertCepToAddress("85960000");
-        address2.setId(UUID.randomUUID());
+        address2.setId(UUID.fromString("9171004e-fb07-4cb1-bbbd-6010418481d7"));
         return address2;
 
       default:
         Address address3 = CepService.convertCepToAddress("85851000");
-        address3.setId(UUID.randomUUID());
+        address3.setId(UUID.fromString("9171004e-fb07-4cb1-bbbd-6010418481d8"));
         return address3;
     }
   }
@@ -53,9 +53,14 @@ public class AddressCreator {
 
   public static Address createUpdatedAddress() throws Exception {
     Address address = createAddressNoId(1);
-    address.setCity("Toledo");
-    address.setNeighborhood("Conjunto Libra");
-    address.setStreet("Rua Ipanema");
+
+    address.setCep("85851-010");
+
+    Address updatedAddress = createAddressNoId(2);
+
+    address.setCity(updatedAddress.getCity());
+    address.setNeighborhood(updatedAddress.getNeighborhood());
+    address.setStreet(updatedAddress.getStreet());
 
     return address;
   }
